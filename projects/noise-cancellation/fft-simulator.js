28a15385a32bcbfe6bce5c5f09f10205ff6c641d
Hila Shmuel <hila4321@gmail.com>
2019-11-08 17:15:54 +0200

    add text

--- projects/noise-cancellation/fft-simulator.js
+++ projects/noise-cancellation/fft-simulator.js
@@ -1,24 +1,11 @@
 audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-
-// shim layer with setTimeout fallback
-window.requestAnimFrame = (function(){
-return  window.requestAnimationFrame       || 
-  window.webkitRequestAnimationFrame || 
-  window.mozRequestAnimationFrame    || 
-  window.oRequestAnimationFrame      || 
-  window.msRequestAnimationFrame     || 
-  function( callback ){
-  window.setTimeout(callback, 1000 / 60);
-};
-})();
-
-drawGraph = (canvas, canvasCtx, bufferLength, dataArray) => {
+drawGraph = (canvas, canvasCtx, bufferLength, dataArray, flip=false) => {
     canvasCtx.fillStyle = "WhiteSmoke";
     canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
   
-    canvasCtx.lineWidth = 1.5;
-    canvasCtx.strokeStyle = "purple";
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = "black";
     canvasCtx.beginPath();
   
     var sliceWidth = (canvas.width * 1.0) / bufferLength;
@@ -26,6 +13,9 @@ drawGraph = (canvas, canvasCtx, bufferLength, dataArray) => {
     for (var i = 0; i < bufferLength; i++) {
       var v = dataArray[i] / 128.0;
       var y = (v * canvas.height) / 2.0;
+      if (flip) {
+        y = canvas.height - y;
+      }
       if (i === 0) {
         canvasCtx.moveTo(x, y);
       } else {
@@ -37,7 +27,7 @@ drawGraph = (canvas, canvasCtx, bufferLength, dataArray) => {
     canvasCtx.lineTo(canvas.width, canvas.height / 2);
     canvasCtx.stroke();
   };
-  
+
 class OscillatorFFT {
   constructor() {
     this.canvasTime = document.querySelector(".time-domain");
@@ -48,7 +38,7 @@ class OscillatorFFT {
   play = () => {
     this.oscillator = audioCtx.createOscillator();
     this.oscillator.type = 'sine'
-    this.oscillator.frequency.value = 400;
+    this.oscillator.frequency.value = 440;
     this.analyser = audioCtx.createAnalyser();
     this.oscillator.connect(this.analyser);
     this.analyser.connect(audioCtx.destination);
@@ -63,6 +53,18 @@ class OscillatorFFT {
   toggle = () => {
     (this.isPlaying ? this.stop() : this.play());
     this.isPlaying = !this.isPlaying;
+  };
+
+  changeFrequency = (value) => {
+    this.oscillator.frequency.value = value;
+  };
+  
+  changeDetune = (value) => {
+    this.oscillator.detune.value = value;
+  };
+  
+  changeType = (type) =>{
+    this.oscillator.type = type;
   };
 
   visualize = () => {
@@ -83,8 +85,21 @@ class OscillatorFFT {
       this.analyser.getByteTimeDomainData(timeBuffer);
       this.analyser.getByteFrequencyData(frequencyBuffer);
       drawGraph(this.canvasTime, canvasCtxTime, timeBufferLength, timeBuffer);
-      drawGraph(this.canvasFrequency, canvasCtxFrequency, frequencyBufferLength, frequencyBuffer);
-    }.bind(this);;
+      drawGraph(this.canvasFrequency, canvasCtxFrequency, frequencyBufferLength, frequencyBuffer, true);
+    }.bind(this); // nice solution for requestAnimationFrame + this:  https://stackoverflow.com/a/32834390
     this.draw();
   }
 }
+
+/*
+# sample rate:  fs
+fs = 44100
+BL = 2048
+fn = fn /2 = 44100 / 2 # max frequency
+Measurement duration D. The measurement duration is given by the sampling rate fs and the blocklength BL.
+D = BL / fs = 2048 / 44100 ~  21.33 ms
+Frequency resolution df. The frequency resolution indicates the frequency spacing between two measurement results.
+
+df = fs / BL = 44100 / 2048 =  21.533203125
+*/
+
